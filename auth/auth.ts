16e4859b5a89554847f250b9cf68f26afe81899c
austinh123123 <88354330+austinh123123@users.noreply.github.com>
2023-03-31 11:45:01 -0700

    Added a log of the token

--- auth/auth.ts
+++ auth/auth.ts
@@ -5,11 +5,15 @@ import { authorize } from "react-native-app-auth";
 const signIn = async () => {
     try {
         const authState = await authorize(authConfig); 
+        console.log(authState);
+        //parse the id_token
+        
     } catch (error) {
         console.log(error);
         //@ts-ignore
         console.log(error.message);
     }   
+
 };
 
 export {signIn}
